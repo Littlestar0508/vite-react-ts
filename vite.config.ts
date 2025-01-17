99c3dc45319bb4da09bf59896a80ff3b1638307e
Littlestar0508 <rist9860@gmail.com>
2025-01-17 15:30:04 +0900

    typescript 초기 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,20 @@
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 const viteConfig = defineConfig({
+  plugins: [
+    react({
+      jsxRuntime: "automatic",
+    }),
+  ],
   server: {
     host: "localhost",
     port: 3000,
   },
-
   preview: {
     host: "localhost",
     port: 8080,
   },
-
   css: {
     devSourcemap: true,
   },
