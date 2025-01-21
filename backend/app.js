386e0fbe78c324c91e2f42108575268460ce1e25
Littlestar0508 <rist9860@gmail.com>
2025-01-21 14:19:17 +0900

    이미지 파일 업로드

--- backend/app.js
+++ backend/app.js
@@ -1,88 +1,105 @@
 import express from 'express';
+import { resolve } from 'node:path';
+import fileUpload from 'express-fileupload';
 import { createUser, isRegisteredUser } from './lib/user.js';
 
-// Express {}
 const app = express();
-// url parser
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static(resolve('./public')));
+app.use(fileUpload());
 
-app.post('/api/signin', async (req, res) => {
-  const { useremail, userpassword } = req.body;
+app.post('/api/signin', async (request, response) => {
+  const { useremail, userpassword } = request.body;
 
   if (!useremail || !userpassword) {
-    return res
+    return response
       .status(400)
-      .send('로그인을 시도하려면 이메일과 비밀번호 둘 다 입력해주세요');
+      .send('로그인을 시도하려면 이메일, 패스워드 입력이 필요합니다.');
   }
 
-  const result = await isRegisteredUser(useremail, userpassword); // True of False
+  const result = await isRegisteredUser(useremail, userpassword); // null or true or false
 
+  console.log({ result });
+
+  // null인 경우, 가입한 적이 없는 사용자 실패!
   if (result === null) {
-    return res.status(400).send(`
-        <p>${useremail} 계정으로 회원가입된 적이 없습니다</p>
-      `);
+    return response.status(400).send(`
+      <p>${useremail} 이메일 계정으로 가입된 적이 없습니다.</p>
+    `);
   }
 
   if (result) {
-    // true인 경우 패스워드도 일치하여 인증 성공
-    return res.status(200).send(`<p>환영합니다 ${useremail}님</p>`);
+    // true인 경우, 패스워드가 유효한 사용자 (인증) 성공!
+    return response.status(200).send(`
+      <p>${useremail} 계정으로 로그인 되었습니다.</p>
+    `);
   } else {
-    return res.status(400).send(`
-        <p>${useremail} 계정 패스워드가 잘못되었습니다</p>
-      `);
+    // false인 경우, 패스워드가 유효하지 않은 사용자 실패!
+    return response.status(400).send(`
+      <p>${useremail} 계정 패스워드가 잘못되었습니다.</p>
+    `);
   }
-
-  res.status(200).send('로그인 성공');
 });
 
-app.post('/api/signup', async (req, res) => {
-  // request.body를 통해 client 요청된 데이터를 받는다
-  const { username, useremail, userpassword } = req.body;
+app.post('/api/signup', async (request, response) => {
+  const { username, useremail, userpassword } = request.body;
 
   if (!username || !useremail || !userpassword) {
-    return res.status(400).send(`
-  <p style="color:red; font-size:48px">회원 가입에 필요한 정보를 모두 입력해주세요</p>
-  `);
+    return response.status(400).send(`
+      <p style="color: red">회원가입에 필요한 이름, 이메일, 패스워드 모두 입력이 필요합니다.</p>
+    `);
   }
 
-  // 새 사용자 생성 (백엔드 스토리지)
+  // 파일(files) 정보 접근
+  const profileImage = request.files?.userprofile;
+  let profileImagePath = '';
+
+  if (profileImage) {
+    await profileImage.mv(resolve('public/files', profileImage.name));
+    profileImagePath = `/files/${profileImage.name}`;
+  } else {
+    console.log('이미지 없음');
+  }
+
   try {
+    // 새 사용자 생성 (백엔드 스토리지)
     const newUser = await createUser({
       name: username,
       email: useremail,
       password: userpassword,
+      profileImage: profileImagePath,
     });
-    console.log(newUser);
 
     if (newUser) {
-      res
-        .status(200)
-        .send(`<p>${newUser.name}님 회원 가입에 성공했습니다.</p>`);
+      // response
+      //   .status(201)
+      //   .send(`${newUser.name}님! 회원가입에 성공했습니다. 😊`);
+      const { password, ...user } = newUser;
+      response.status(201).json(user);
     } else {
-      res.status(400).send(`<p>${useremail}은 이미 가입된 이메일입니다</p>`);
+      response
+        .status(400)
+        .send(`${username}님은 회원 가입을 이미 하셨습니다. 😥`);
     }
-  } catch (err) {
-    res.status(400).send('새 사용자 생성에 문제가 발생했습니다.');
+  } catch (error) {
+    response.status(500).send('회원가입에 문제가 발생했습니다.');
   }
 });
 
-// 라우팅
 app.get('/api/hello', (request, response) => {
   const { username, useremail } = request.query;
-
   if (username && useremail) {
-    response
-      .status(200)
-      .send(
-        `<h1>hello ${username}!</h1> <p>your email address is ${useremail}</p>`
-      );
+    response.status(200).send(`
+      <h1>hello ${username}!</h1>
+      <p>your email address is ${useremail}</p>
+    `);
   } else {
     response
       .status(400)
-      .send(`<p>사용자 이름과 이메일이 전송되지 않았습니다</p>`);
+      .send('<p>사용자 이름과 이메일이 전송되지 않았습니다. 😥</p>');
   }
 });
 
 app.listen(4000, () => {
-  console.log('백엔드 프로그램 서버 구동 http://localhost:4000');
+  console.log('백엔드 프로그램 서버 구동 http://localhost:4000/api/hello');
 });
