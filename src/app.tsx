de99ed01151046ceb3e0597bc770723ee3a7c758
Littlestar0508 <rist9860@gmail.com>
2025-01-24 16:22:39 +0900

    로그인, 회원가입 구현

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,22 @@
+import { useState } from 'react';
 import SignUpForm from './playground/signup-form';
-function App() {
+import SignInForm from './playground/signin-form';
+
+function Playground() {
+  const [view, setView] = useState(false);
+
   return (
-    <>
-      <SignUpForm></SignUpForm>
-    </>
+    <div className="Playground">
+      <h1>플레이그라운드</h1>
+      {view ? <SignInForm /> : <SignUpForm />}
+
+      <hr />
+
+      <button type="button" onClick={() => setView(!view)}>
+        {view ? '회원가입' : '로그인'} 페이지로 이동
+      </button>
+    </div>
   );
 }
 
-export default App;
+export default Playground;
