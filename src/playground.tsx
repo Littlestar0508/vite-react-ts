333b2a1f1867f1c72ec15a64e824334fe0170bcc
Littlestar0508 <rist9860@gmail.com>
2025-02-03 17:40:53 +0900

    switch문을 사용한 네이게이션 재구성
    파생된 상태를 이용

--- src/playground.tsx
+++ src/playground.tsx
@@ -1,30 +1,36 @@
 import { useState } from 'react';
 import Nav from '@/homework/components/nav';
-import HomeworkSignInForm from '@/homework/pages/sign-in';
-import HomeworkSignUpForm from '@/homework/pages/sign-up';
+import SignInForm from '@/homework/pages/sign-in';
+import SignUpForm from '@/homework/pages/sign-up';
 import { getUIView, type UIView } from '@/homework/lib/ui-view';
-import DS_Button from './data/design-system/button';
+import StateManagement from '@/homework/pages/state-management';
 
 function Playground() {
   const [uiView] = useState<UIView>(getUIView);
-  const isSignInView = uiView.includes('signin');
+
+  let ViewComponent: React.ReactElement | null = null;
+
+  switch (uiView) {
+    case 'signin': {
+      ViewComponent = <SignInForm />;
+      break;
+    }
+    case 'signup': {
+      ViewComponent = <SignUpForm />;
+      break;
+    }
+    case 'state-management': {
+      ViewComponent = <StateManagement />;
+      break;
+    }
+  }
 
   return (
     <div className="Playground bg-euid-gray-200 wrapper">
       <h1>플레이그라운드</h1>
       <Nav />
-      <article className="flex space-x-2 my-10 items-center justify-center">
-        <h2>컴포넌트 변형(variants)</h2>
-        <div role="group" className="flex gap-4 mt-4">
-          <DS_Button>저장</DS_Button>
-          <DS_Button size="small">버튼</DS_Button>
-          <DS_Button intent="secondary">취소</DS_Button>
-          <DS_Button intent="secondary" disabled>
-            편집
-          </DS_Button>
-        </div>
-      </article>
-      {isSignInView ? <HomeworkSignInForm /> : <HomeworkSignUpForm />}
+
+      {ViewComponent}
     </div>
   );
 }
