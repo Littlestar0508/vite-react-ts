60934474746633f78ef7cb7be54edc9dafdb34fb
Littlestar0508 <rist9860@gmail.com>
2025-02-04 16:19:08 +0900

    상태 변수를 최소화하는 방법
    - 배열을 사용한 상태 업데이트
    - 파생된 상태는 배열을 이용하여 생성

--- src/homework/pages/tic-tac-toe/components/grid.tsx
+++ src/homework/pages/tic-tac-toe/components/grid.tsx
@@ -5,28 +5,27 @@ import { type Cells, INITIAL_CELLS, PLAYER } from '../constant';
 
 export default function Grid() {
   // 상태(게임 보드 3x3)
-  const [cells] = useState<Cells>(INITIAL_CELLS);
-  const [order, setOrder] = useState<number>(0);
+  const [cells, setCells] = useState<Cells>(INITIAL_CELLS);
+
+  const length = cells.filter((cell) => Boolean(cell)).length;
 
   // [파생된 상태]
   // 다음 플레이어는?
-  const nextPlayer = order % 2 === 0 ? PLAYER.ONE : PLAYER.TWO;
+  const nextPlayer = length % 2 === 0 ? PLAYER.ONE : PLAYER.TWO;
 
   // [이벤트 핸들러]
   // 게임 상태 업데이트
   const handlePlay = (index: number) => {
-    const nextOrder = order + 1;
-    setOrder(nextOrder);
-
-    console.log(index, 'click', nextPlayer);
+    const nextCells = cells.map((cell, i) => (index !== i ? cell : nextPlayer));
+    setCells(nextCells);
   };
 
   return (
     <div className={tm('grid grid-rows-3 grid-cols-3 gap-1')}>
-      {cells.map((_cell, index) => {
+      {cells.map((cell, index) => {
         return (
           <Cell key={index} onPlay={() => handlePlay(index)}>
-            {index}
+            {cell}
           </Cell>
         );
       })}
