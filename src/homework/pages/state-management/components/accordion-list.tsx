e548ef051ae41773ec0bd804ceff4ad1e99d0691
Littlestar0508 <rist9860@gmail.com>
2025-02-04 09:31:14 +0900

    상태관리전략 학습 목적의 컴포넌트 구성
    - AccordionList
    - AccordionItem
    - AccordionOpenedCount

---
+++ src/homework/pages/state-management/components/accordion-list.tsx
@@ -0,0 +1,31 @@
+import AccordionItem from './accordion-item';
+import { AccordionOpenedCount } from './accordion-opened-count';
+
+interface AccordionListProps {
+  title: string;
+}
+
+export default function AccordionList({ title }: AccordionListProps) {
+  return (
+    <article>
+      <h3 className="sr-only">{title}</h3>
+      <AccordionOpenedCount />
+      <AccordionItem title="인생이란 무엇인가..">
+        <p>현재는 오후 5시 44분..</p>
+        <p>곧 수업이 끝나간다</p>
+        <p>오늘은 어떤 내용을 복습할까</p>
+        <p>운동도 가야지</p>
+        <p>저녁도 먹어야지</p>
+        <p>바쁘다 바빠 현대 사회</p>
+      </AccordionItem>
+      <AccordionItem title="현대 사회">
+        <p>지금은 오후 5시 51분..</p>
+        <p>곧 수업이 끝나간다</p>
+        <p>오늘은 어떤 내용을 복습할까</p>
+        <p>운동도 가야지</p>
+        <p>저녁도 먹어야지</p>
+        <p>바쁘다 바빠 현대 사회</p>
+      </AccordionItem>
+    </article>
+  );
+}
