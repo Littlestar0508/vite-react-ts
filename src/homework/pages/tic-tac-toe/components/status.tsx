cf2cebf54d9a7c2360be5b445f7c98daa544f464
Littlestar0508 <rist9860@gmail.com>
2025-02-06 13:40:10 +0900

    게임 초기화 함수 작성

--- src/homework/pages/tic-tac-toe/components/status.tsx
+++ src/homework/pages/tic-tac-toe/components/status.tsx
@@ -1,9 +1,37 @@
+import { tm } from '@/utils/tw-merge';
+
 interface StatusProps {
   message: string;
+  onReGame?: () => void;
 }
 
-function Status({ message }: StatusProps) {
-  return <div>{message}</div>;
+function Status({ message, onReGame }: StatusProps) {
+  // 파생된 상태
+  const isComplete = !message.includes('넥스트 플레이어');
+
+  const handleReGame = () => {
+    onReGame?.();
+  };
+
+  return (
+    <div className={tm('flex justify-between w-full px-5')}>
+      <p>{message}</p>
+      {isComplete && (
+        <button
+          type="button"
+          onClick={handleReGame}
+          className={tm(
+            'cursor-pointer',
+            'px-3 py-1 border-1 border-slate-400 rounded-sm text-xs',
+            'text-xs font-semibold',
+            'hover:bg-slate-800/10'
+          )}
+        >
+          다시 하기
+        </button>
+      )}
+    </div>
+  );
 }
 
 export default Status;
