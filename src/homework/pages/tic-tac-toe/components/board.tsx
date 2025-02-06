9f390e1238083f61c4dbb6901c0e5a4ac7779068
Littlestar0508 <rist9860@gmail.com>
2025-02-06 14:13:45 +0900

    상태 끌어올리기(index로)
    게임 진행에 따른 히스토리 리스트 렌더링

--- src/homework/pages/tic-tac-toe/components/board.tsx
+++ src/homework/pages/tic-tac-toe/components/board.tsx
@@ -1,64 +1,28 @@
-import { useState } from 'react';
 import { tm } from '@/utils/tw-merge';
-import {
-  getNextPlayer,
-  getStatusMessage,
-  getWinner,
-  INITIAL_CELLS,
-  type Cells,
-} from '../constant';
+import { type Winner, type Cells } from '../constant';
 import Status from './status';
 import Grid from './grid';
 
-function Board() {
-  // [상태]
-  // 게임 보드 셀(cells, 9개(3 x 3))
-  const [cells, setCells] = useState<Cells>(INITIAL_CELLS);
-  // 게임 순서(order)
-  const [order, setOrder] = useState<number>(0);
+interface BoardProps {
+  cells: Cells;
+  winner: Winner;
+  statusMessage: string;
+  onReGame?: () => void;
+  onPlayGame?: (cellIndex: number) => void;
+}
 
-  // [파생된 상태]
-  // 다음 플레이어(Next Player)는?
-  const nextPlayer = getNextPlayer(order);
-
-  // 현재 게임 턴에 승리자(Winner)가 있나요?
-  const winner = getWinner(cells);
-
-  // [이벤트 핸들러]
-  // 게임 진행 함수
-  const handlePlay = (index: number) => {
-    // 승리자가 있다면 게임 오버(GAME OVER)
-    if (winner) {
-      // 알림(notification)
-      alert(`GAME OVER!\nWinner ${winner.player}`);
-      return;
-    }
-
-    // 승리자가 없다면 게임 진행 계속
-
-    // 게임 상태 업데이트 (순서)
-    const nextOrder = order + 1;
-    setOrder(nextOrder);
-
-    // 게임 상태 업데이트 (게임 보드 셀)
-    const nextCells = cells.map((cell, i) => (index !== i ? cell : nextPlayer));
-    setCells(nextCells);
-  };
-
-  // [파생된 상태]
-  // 게임 상태 메시지
-  const statusMessage = getStatusMessage(nextPlayer, winner, cells);
-
-  const handleReGame = () => {
-    setCells(INITIAL_CELLS);
-    setOrder(0);
-  };
-
+function Board({
+  cells,
+  winner,
+  statusMessage,
+  onReGame,
+  onPlayGame,
+}: BoardProps) {
   return (
     <section className={tm('flex flex-col space-y-2 items-center', 'w-60')}>
       <h3 className="sr-only">게임 보드</h3>
-      <Status message={statusMessage} onReGame={handleReGame} />
-      <Grid cells={cells} winner={winner} onPlay={handlePlay} />
+      <Status message={statusMessage} onReGame={onReGame} />
+      <Grid cells={cells} winner={winner} onPlay={onPlayGame} />
     </section>
   );
 }
--- src/homework/pages/tic-tac-toe/components/history.tsx
+++ src/homework/pages/tic-tac-toe/components/history.tsx
@@ -1,12 +1,20 @@
 import { tm } from '@/utils/tw-merge';
 import HistoryItem from './history-item';
 
-export default function History() {
+interface HistoryProps {
+  count: number;
+}
+
+export default function History({ count }: HistoryProps) {
+  const historyCount = Array(count).fill(null);
+
   return (
     <section className={tm('flex flex-col space-y-2 items-center', 'w-40')}>
       <h3>게임 히스토리</h3>
       <ol className={tm('flex flex-col space-y-1')}>
-        <HistoryItem />
+        {historyCount.map((_, index) => (
+          <li key={index}>{index}</li>
+        ))}
       </ol>
     </section>
   );
