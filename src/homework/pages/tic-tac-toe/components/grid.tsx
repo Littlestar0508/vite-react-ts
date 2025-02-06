ad2d432775274d36dd59ea2b3f3b57d8484ea5f1
Littlestar0508 <rist9860@gmail.com>
2025-02-06 11:36:12 +0900

    게임 종료 로직 구현
    - 게임 종료 시 색상 채우기
    - 게임 종료 후 클릭하면 승자 알림

--- src/homework/pages/tic-tac-toe/components/grid.tsx
+++ src/homework/pages/tic-tac-toe/components/grid.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import Cell from './cell';
 import { tm } from '@/utils/tw-merge';
-import { type Cells, INITIAL_CELLS, PLAYER } from '../constant';
+import { type Cells, getWinner, INITIAL_CELLS, PLAYER } from '../constant';
 
 export default function Grid() {
   // 상태(게임 보드 3x3)
@@ -13,9 +13,19 @@ export default function Grid() {
   // 다음 플레이어는?
   const nextPlayer = length % 2 === 0 ? PLAYER.ONE : PLAYER.TWO;
 
+  // 현재 이번 턴에 게임 승리자가 있나요?
+  const winner = getWinner(cells);
+
   // [이벤트 핸들러]
   // 게임 상태 업데이트
   const handlePlay = (index: number) => {
+    // 승리자가 있다면 game over
+    if (winner) {
+      alert(`GAME OVER!\nWinner ${winner.player}`);
+      return;
+    }
+
+    // 없다면 정상 진행
     const nextCells = cells.map((cell, i) => (index !== i ? cell : nextPlayer));
     setCells(nextCells);
   };
@@ -23,8 +33,20 @@ export default function Grid() {
   return (
     <div className={tm('grid grid-rows-3 grid-cols-3 gap-1')}>
       {cells.map((cell, index) => {
+        let winnerClass = '';
+        if (winner) {
+          const [x, y, z] = winner.condition;
+          if (index === x || index === y || index === z) {
+            winnerClass = 'bg-blue-700/30 outline-3 outline-blue-700';
+          }
+        }
+
         return (
-          <Cell key={index} onPlay={() => handlePlay(index)}>
+          <Cell
+            key={index}
+            onPlay={() => handlePlay(index)}
+            className={winnerClass}
+          >
             {cell}
           </Cell>
         );
--- src/homework/pages/tic-tac-toe/constant.ts
+++ src/homework/pages/tic-tac-toe/constant.ts
@@ -12,3 +12,40 @@ export enum PLAYER {
 export type Player = null | PLAYER.ONE | PLAYER.TWO;
 
 export type Cells = Player[];
+
+// 게임의 승리 조건
+const WIN_CONDITION = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
+type Winner = {
+  player: PLAYER;
+  condition: [number, number, number];
+} | null;
+
+export const getWinner = (cells: Cells): Winner => {
+  let winner = null;
+
+  // 승리조건 순환
+  for (const [i, j, k] of WIN_CONDITION) {
+    // 승리 조건에 해당하는 위치에 말판이 일치하는지 검사-
+    const player = cells.at(i);
+
+    if (player && cells.at(j) === player && cells.at(k) === player) {
+      winner = {
+        player,
+        condition: [i, j, k],
+      };
+      break;
+    }
+  }
+
+  return winner as Winner;
+};
