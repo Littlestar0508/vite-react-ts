13af5efd573903755343a9884a45cecbba977d44
Littlestar0508 <rist9860@gmail.com>
2025-02-07 14:16:56 +0900

    오류 경계
    - componentDidCatch 메서드를 활용
    - 오류 발생 시 오류가 발생한 컴포넌트 스택 정보 표시

--- src/components/error-boundary/index.tsx
+++ src/components/error-boundary/index.tsx
@@ -18,23 +18,37 @@ class ErrorBoundary extends Component<Props, State> {
   };
 
   // 오류 감지하기 위한 라이프 사이클 메서드
-  static getDerivedStateFromError(error: Error) {
-    // 상태 업데이트
-    return {
+  // static getDerivedStateFromError(error: Error) {
+  // 상태 업데이트
+  //   return {
+  //     hasError: true,
+  //     error,
+  //   };
+  // }
+
+  componentDidCatch(error: Error, errorInfo: ErrorInfo): void {
+    this.setState({
       hasError: true,
+      errorInfo,
       error,
-    };
+    });
   }
 
   render() {
     // 대체(fallback) UI를 렌더링(Error O)
     if (this.state.hasError) {
-      const { error } = this.state;
+      const { error, errorInfo } = this.state;
 
       return (
-        <div role="alert" lang="en">
-          <h2>{error?.name} 오류 발생!!!</h2>
-          <p>{error?.message}</p>
+        <div
+          role="alert"
+          className="overflow-scroll flex flex-col gap-2 border-4 border-red-600 text-red-600 p-5"
+        >
+          <h2 className="text-xl font-semibold">{error?.name} 오류 발생!!!</h2>
+          <p className="text-red-700 font-normal">{error?.message}</p>
+          <pre className="-ml-8 text-xs leading-[2]">
+            {errorInfo?.componentStack}
+          </pre>
         </div>
       );
     }
