cfcca9e3678da4027935763d3272b100956303ee
Littlestar0508 <rist9860@gmail.com>
2025-02-07 13:39:28 +0900

    클래스 vs 함수 컴포넌트
    - 상태
    -파생된 상태
    - 상태 업데이트(이벤트 핸들러)

---
+++ src/pages/error-boundary/counter.tsx
@@ -0,0 +1,62 @@
+import { useState } from 'react';
+import { tm } from '@/utils/tw-merge';
+
+interface CounterProps {
+  count?: number;
+  step?: number;
+  min?: number;
+  max?: number;
+}
+
+export default function Counter({
+  count: initialCount = 0,
+  step = 1,
+  min = 0,
+  max = 10,
+}: CounterProps) {
+  // 상태
+  const [count, setCount] = useState(initialCount);
+
+  // 파생된 상태
+  const doubleCount = count * 2;
+
+  // 이벤트 핸들러
+  const handleDecrease = () => {
+    const nextCount = count - step;
+    if (nextCount < min) return;
+
+    setCount(nextCount);
+  };
+
+  const handleIncrease = () => {
+    const nextCount = count + step;
+    if (nextCount > max) return;
+
+    setCount(nextCount);
+  };
+
+  // 렌더
+  return (
+    <div className={tm('flex flex-col gap-2 items-start')}>
+      <output className={tm('font-semibold text-3xl text-react')}>
+        {count} {doubleCount}
+      </output>
+      <div className={tm('flex', '*:hover:bg-sky-800 *:cursor-pointer')}>
+        <button
+          type="button"
+          className={tm('px-6 py-1 bg-react text-white rounded-l-full')}
+          onClick={handleDecrease}
+        >
+          -{step}
+        </button>
+        <button
+          type="button"
+          className={tm('px-6 py-1 bg-react text-white rounded-r-full')}
+          onClick={handleIncrease}
+        >
+          +{step}
+        </button>
+      </div>
+    </div>
+  );
+}
