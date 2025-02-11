4dd8a87197d70649b590c61d377bd34021bfd686
Littlestar0508 <rist9860@gmail.com>
2025-02-11 16:02:07 +0900

    파생된 상태를 이용한 검색 목록

--- src/pages/search-list/components/searched-list.tsx
+++ src/pages/search-list/components/searched-list.tsx
@@ -23,15 +23,27 @@ export default function SearchedList({
       item.description.includes(word) ||
       item.tags.includes(word)
   );
+  const filteredCount = filteredList.length;
+  const isEmpty = filteredCount === 0;
 
   return (
     <section>
       <h3 className="sr-only">검색된 리스트</h3>
-      <ul className={tm('flex flex-col gap-12')}>
-        {filteredList.map((item) => (
-          <Card item={item} key={item.id} onUpdate={onUpdate} />
-        ))}
-      </ul>
+      {isEmpty && (
+        <p className="text-lg text-slate-700 mb-5">검색된 정보가 없습니다</p>
+      )}
+      {!isEmpty && (
+        <>
+          <p className="text-lg text-slate-700 text-center mb-5">
+            {filteredCount}개 검색
+          </p>
+          <ul className={tm('flex flex-col gap-12')} aria-hidden={isEmpty}>
+            {filteredList.map((item) => (
+              <Card item={item} key={item.id} onUpdate={onUpdate} />
+            ))}
+          </ul>
+        </>
+      )}
     </section>
   );
 }
