3c5d66f775b5273265963687053f03f42f5ceef8
Littlestar0508 <rist9860@gmail.com>
2025-02-11 14:27:34 +0900

    카드 컴포넌트 구성 - 아이콘 라이브러리 사용 - 즐겨찾기 버튼 추가

--- src/pages/search-list/components/card.tsx
+++ src/pages/search-list/components/card.tsx
@@ -1,33 +1,53 @@
 import { tm } from '@/utils/tw-merge';
+import { Heart, HeartSolid } from '@mynaui/icons-react';
 import { ColorMoodItem } from '../types';
 import generateGradient from '../utils/generate-gradient';
+import { useState } from 'react';
 
 interface CardProps {
   item: ColorMoodItem;
 }
 
 export default function Card({ item }: CardProps) {
+  const [isFavorited] = useState(false);
+
   const handleLink = (e: React.MouseEvent<HTMLAnchorElement>) => {
     e.preventDefault();
     console.log(slug);
   };
 
   const slug = `/color-mood/${item.id}`;
-
+  const iconSize = 36;
+  const buttonLabel = `즐겨찾기 ${isFavorited ? '제거' : '추가'}`;
+  const gradientStyles = { background: generateGradient(item.id) };
+  const Icon = isFavorited ? HeartSolid : Heart;
   return (
-    <li className={tm('flex flex-col items-center')}>
+    <li className={tm('flex flex-col items-center gap-4')}>
       <figure
         role="presentation"
-        className={tm('size-32 rounded-full')}
-        style={{ background: generateGradient(item.id) }}
-      ></figure>
+        className={tm('size-32 rounded-full', 'relative')}
+        style={gradientStyles}
+      >
+        <button
+          type="button"
+          title={buttonLabel}
+          aria-label={buttonLabel}
+          className={tm(
+            'cursor-pointer',
+            'rounded-full',
+            'absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'
+          )}
+        >
+          <Icon size={iconSize} />
+        </button>
+      </figure>
       <a
         href={slug}
-        className={tm('flex flex-col items-center')}
+        className={tm('flex flex-col items-center gap-0')}
         onClick={handleLink}
       >
-        <h4>{item.title}</h4>
-        <p>{item.description}</p>
+        <h4 className="text-[22px] font-semibold">{item.title}</h4>
+        <p className="text-sm text-slate-700">{item.description}</p>
       </a>
     </li>
   );
--- src/pages/search-list/components/searched-list.tsx
+++ src/pages/search-list/components/searched-list.tsx
@@ -10,7 +10,7 @@ export default function SearchedList({ list }: SearchedListProps) {
   return (
     <section>
       <h3 className="sr-only">검색된 리스트</h3>
-      <ul className={tm('flex flex-col gap-4 ')}>
+      <ul className={tm('flex flex-col gap-12')}>
         {list.map((item) => (
           <Card item={item} key={item.id} />
         ))}
