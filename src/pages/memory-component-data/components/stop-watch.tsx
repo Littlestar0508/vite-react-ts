4caf16fd1994cd310fde445a35fd2e7a373dc47a
Littlestar0508 <rist9860@gmail.com>
2025-02-12 15:06:55 +0900

    타입 포매팅 설정, startTime과 nowTime 초기화 버그 수정

--- src/pages/memory-component-data/components/stop-watch.tsx
+++ src/pages/memory-component-data/components/stop-watch.tsx
@@ -1,11 +1,32 @@
 import { tm } from '@/utils/tw-merge';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 // FPS
 const FRAME = 1000 / 60;
 
 // 함수 호출 시점의 현재 시간 값 반환 함수
 const getTime = () => Date.now();
+
+// 타임 포맷팅 함수(반환 값 : "HH:MM:SS:MS")
+const formatTime = (time: number) => {
+  // 밀리초(miliseconds)
+  const miliseconds = parseInt(`${time % 100}`, 10);
+  // 초(seconds) = 1000ms
+  const seconds = parseInt(`${(time / 1000) % 60}`, 10);
+  // 분(minutes) = 60s
+  const minutes = parseInt(`${(time / (1000 * 60)) % 60}`, 10);
+  // 시(hours) = 60m
+  const hours = parseInt(`${(time / (1000 * 60 * 60)) % 60}`, 10);
+
+  const [hh, mm, ss, ms] = [hours, minutes, seconds, miliseconds].map(
+    (time) => {
+      return time.toLocaleString('ko-KR', {
+        minimumIntegerDigits: 2,
+      });
+    }
+  );
+  return `${hh}:${mm}:${ss}:${ms}`;
+};
 
 export default function StopWatch() {
   // 상태
@@ -26,21 +47,23 @@ export default function StopWatch() {
     setNowTime(getTime);
   };
 
+  // 렌더링과 무관한 데이터 기억 (브라우저 API : 사이드 이펙트)
+  const clearIntervalIdRef = useRef<ReturnType<typeof setInterval>>(undefined);
+
   // 이펙트 - 외부 시스템인 브라우저 API와 동기화
   useEffect(() => {
-    let clearIntervalId: ReturnType<typeof setInterval> | number = 0;
-
     if (isStart) {
       resetTime();
-      clearIntervalId = setInterval(() => {
+      clearIntervalIdRef.current = setInterval(() => {
         setNowTime(getTime);
       }, FRAME);
     } else {
-      clearInterval(clearIntervalId);
+      clearInterval(clearIntervalIdRef.current);
       // 기록 시간 업데이트
       setRecordTime((recordTime) => {
         return recordTime + nowTime - startTime;
       });
+      resetTime();
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [isStart]);
@@ -56,10 +79,15 @@ export default function StopWatch() {
     resetTime();
   };
 
+  // 파생된 상태
+  // 타임 포맷팅
+  // "HH:MM:SS:MS"
+  const timeInfo = formatTime(recordTime + nowTime - startTime);
+
   return (
     <article aria-label="스톱워치" className="flex flex-col gap-2">
       <time className="px-6 py-2 bg-black text-white text-2xl font-mono text-center w-80">
-        {recordTime + nowTime - startTime}
+        {timeInfo}
       </time>
       <div className="flex gap-3">
         <button
