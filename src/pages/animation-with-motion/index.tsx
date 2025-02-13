43478f9faca476dcbefbe086b4c2671c17082291
Littlestar0508 <rist9860@gmail.com>
2025-02-13 09:43:22 +0900

    animation replay 버튼 구현
    - React 컴포넌트 초기화는 key 속성이 변경되면 진행
    
    - React 컴포넌트는 속성이 변경되면 다시 실행

--- src/pages/animation-with-motion/index.tsx
+++ src/pages/animation-with-motion/index.tsx
@@ -1,13 +1,42 @@
-import { Box } from '@mynaui/icons-react';
+import { Box, Redo } from '@mynaui/icons-react';
 import AnimationBox from './components/animation-box';
+import { tm } from '@/utils/tw-merge';
+import { useState } from 'react';
 
 export default function AnimationWithMotionPage() {
+  // 화면 업데이트를 위한 상태 선언
+  const [replayKey, setReplayKey] = useState(0);
+
+  // 상태 업데이트 확인을 위한 useEffect
+  // useEffect(() => {
+  //   console.log({ replayKey });
+  // }, [replayKey]);
+
+  const handleReplay = () => {
+    setReplayKey((r) => r + 1);
+  };
+
   return (
-    <section className="flex flex-col gap-5">
+    <section className="flex flex-col gap-7 items-start">
       <h2 className="text-2xl font-medium">
         애니메이션 - Motion 라이브러리 활용
       </h2>
-      <AnimationBox>
+      <button
+        type="button"
+        onClick={handleReplay}
+        className={tm(
+          'cursor-pointer',
+          'flex items-center gap-1.5 px-3.5 py-2 rounded-lg',
+          'bg-react text-white text-sm font-semibold uppercase',
+          '*:hover:animate-spin *:duration-100',
+          'active:scale-95'
+        )}
+      >
+        <Redo size={20} />
+        RePlay
+      </button>
+
+      <AnimationBox key={replayKey}>
         <Box size={64} />
       </AnimationBox>
     </section>
