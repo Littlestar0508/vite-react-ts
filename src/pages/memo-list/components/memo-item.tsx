c5949669aeed8813cf07a2a68ae74888e11dc533
Littlestar0508 <rist9860@gmail.com>
2025-02-18 14:34:56 +0900

    메모 수정 기능 구현
    - 사용자 인터페이스 피드백 제공
    - 저장, 수정 상태 선언

--- src/pages/memo-list/components/memo-item.tsx
+++ src/pages/memo-list/components/memo-item.tsx
@@ -1,8 +1,11 @@
 import { tm } from '@/utils/tw-merge';
-import type { MemoItem as MemoItemType } from '../lib/supabase-client';
-import { EditOne, TrashOne } from '@mynaui/icons-react';
-import { deleteMemoItem } from '../lib/api';
-import { useState } from 'react';
+import type {
+  MemoItem as MemoItemType,
+  MemoItemUpdate,
+} from '../lib/supabase-client';
+import { EditOne, EditOneSolid, TrashOne } from '@mynaui/icons-react';
+import { deleteMemoItem, editMemoItem } from '../lib/api';
+import { useEffect, useRef, useState } from 'react';
 import Loading from './loading';
 import delay from '@/utils/delay';
 
@@ -11,6 +14,48 @@ interface MemoItemProps {
 }
 
 export default function MemoItem({ item }: MemoItemProps) {
+  // 수정 상태와 업데이트 핸들러
+  const [isEditing, setIsEditing] = useState(false);
+  const [isSaving, setIsSaving] = useState(false);
+
+  useEffect(() => {
+    if (isEditing) {
+      titleRef.current?.focus();
+      console.log(titleRef.current?.textContent);
+    }
+  }, [isEditing]);
+
+  // 파생된 상태
+  const editButtonLabel = isEditing ? '수정 중...' : '수정';
+
+  const handleChangeEditMode = () => {
+    setIsEditing(true);
+  };
+
+  const handleChangeViewMode = async () => {
+    setIsSaving(true);
+    const titleElement = titleRef.current!;
+    const contentElement = contentRef.current!;
+    const currentTime = new Date().toISOString();
+
+    const willEditMemoItem = {
+      ...item,
+      title: titleElement.textContent,
+      content: contentElement.textContent,
+      updated_at: currentTime,
+    } as MemoItemUpdate;
+
+    await delay(600);
+    await editMemoItem(willEditMemoItem);
+
+    setIsSaving(false);
+    setIsEditing(false);
+  };
+
+  const titleRef = useRef<HTMLHeadingElement>(null);
+  const contentRef = useRef<HTMLParagraphElement>(null);
+
+  // 삭제 상태와 업데이트 핸들러
   const [isDeleting, setIsDeleting] = useState(false);
 
   const handleDelete = async () => {
@@ -25,31 +70,43 @@ export default function MemoItem({ item }: MemoItemProps) {
       key={item.id}
       className={tm(
         'flex flex-col gap-1.5 p-4 bg-react text-white rounded-sm',
-        { 'relative bg-react/35': isDeleting }
+        { 'relative bg-react/35': isDeleting || isSaving }
       )}
     >
-      {isDeleting && (
+      {(isDeleting || isSaving) && (
         <Loading
-          label="삭제 중..."
+          label={isSaving ? '수정 중...' : '삭제 중...'}
           className="absolute top-1/2 left-1/2 -translate-1/2 text-sky-300 size-20"
         />
       )}
-      <h3 className="font-medium tracking-wide text-lg text-sky-300">
+      <h3
+        ref={titleRef}
+        contentEditable={isEditing}
+        suppressContentEditableWarning
+        className="font-medium tracking-wide text-lg text-sky-300"
+      >
         {item.title}
       </h3>
-      <p className="text-sm text-slate-200 leading-relaxed">{item.content}</p>
+      <p
+        ref={contentRef}
+        contentEditable={isEditing}
+        suppressContentEditableWarning
+        className="text-sm text-slate-200 leading-relaxed"
+      >
+        {item.content}
+      </p>
       <div role="group">
         <button
           type="button"
-          aria-label="수정"
-          title="수정"
+          aria-label={editButtonLabel}
+          title={editButtonLabel}
+          onClick={!isEditing ? handleChangeEditMode : handleChangeViewMode}
           className={tm(
             'cursor-pointer',
             'size-6 opacity-75 hover:opacity-100'
           )}
         >
-          <EditOne size={20} />
-          {/* <EditOneSolid size={20} /> */}
+          {isEditing ? <EditOneSolid size={20} /> : <EditOne size={20} />}
         </button>
         <button
           type="button"
