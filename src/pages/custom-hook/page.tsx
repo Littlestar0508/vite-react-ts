67b5453f91b9090bdf6dff859e834a3da9986850
Littlestar0508 <rist9860@gmail.com>
2025-02-19 13:02:38 +0900

    hook 함수와 title태그로 제목 생성

--- src/pages/custom-hook/page.tsx
+++ src/pages/custom-hook/page.tsx
@@ -1,35 +1,35 @@
+import Title from '@/components/title';
+import { useFetchData } from '@/hooks/use-fetch-data';
+import useToggle from '@/hooks/use-toggle';
 import Loading from '../memo-list/components/loading';
-import PokemonWithUseFecthData from './components/pokemon-widthuse-fetch-data';
-import { useFetchData } from '@/hooks/use-fecth';
-import PokemonWithUseQuery from './components/pokemon-with-use-query.tsx';
-
-interface PokemonList {
-  count: number;
-  next: string;
-  previous: string;
-  results: PokemonSimpleData[];
-}
-
-interface PokemonSimpleData {
-  name: string;
-  url: string;
-}
+import PokemonWithUseFetchData from './components/pokemon-with-use-fetch-data';
+import PokemonWithUseQuery from './components/pokemon-with-use-query';
+import type { PokemonList } from './types';
 
 function CustomHookPage() {
   // 포켓몬 집합 정보 가져오기
   // 'https://pokeapi.co/api/v2/pokemon?offset=3&limit=10'
 
   const { loading, error, data } = useFetchData<PokemonList>(
-    'https://pokeapi.co/api/v2/pokemon?offset=3&limit=20'
+    'https://pokeapi.co/api/v2/pokemon?offset=3&limit=28'
   );
+
+  const [toggleState, toggle] = useToggle(true, {
+    // persist: true,
+    key: '@toggle/',
+  });
 
   return (
     <>
-      <title>커스텀 훅</title>
+      <Title>커스텀 훅</Title>
       <section className="flex flex-col gap-2">
         <h2 className="font-medium text-2xl mb-6">
           사용자 정의 훅 함수(Custom Hook) 활용
         </h2>
+
+        <button type="button" onClick={toggle}>
+          {toggleState?.toString()}
+        </button>
 
         <h3 className="text-xl font-medium">
           페이지에서 데이터 요청/응답 후, 화면 업데이트
@@ -39,14 +39,14 @@ function CustomHookPage() {
         )}
         {error && <div role="alert">{error.message}</div>}
         {/* {data && <pre>{JSON.stringify(data, null, 2)}</pre>} */}
-        {data && <output>{data.results.length}</output>}
+        {!loading && data && <output>{data.results.length}</output>}
 
         <hr className="my-10" />
 
         <h3 className="text-xl font-medium mb-6">
           컴포넌트에서 데이터 요청/응답 후, 화면 업데이트
         </h3>
-        <PokemonWithUseFecthData />
+        <PokemonWithUseFetchData />
         <PokemonWithUseQuery />
       </section>
     </>
--- src/pages/memo-list/page.tsx
+++ src/pages/memo-list/page.tsx
@@ -1,14 +1,12 @@
-import { useEffect, useState } from 'react';
+import Title from '@/components/title';
 import type { PostgrestError } from '@supabase/supabase-js';
-import type { MemoItem } from './lib/supabase-client';
-import MemoList from './components/memo-list';
+import { useEffect, useState } from 'react';
 import Loading from './components/loading';
+import MemoList from './components/memo-list';
 import { getMemoList, subscribe } from './lib/api';
-import useDocumentTitle from '@/hooks/use-document-title';
+import type { MemoItem } from './lib/supabase-client';
 
 function MemoListPage() {
-  useDocumentTitle('메모리스트 with Supabase');
-
   const [loading, setLoading] = useState<boolean>(true);
   const [data, setData] = useState<null | MemoItem[]>(null);
   const [error, setError] = useState<null | PostgrestError>(null);
@@ -77,9 +75,7 @@ function MemoListPage() {
 
   return (
     <>
-      {/* useDocumentTitle 커스텀 훅을 사용하지 않고 페이지 별 title 지정 가능 */}
-      {/* 만약 title 태그를 사용하려면 html문서에서 title을 제거해야한다 */}
-      {/* <title>메모리스트 with Supabase | 리액트</title> */}
+      <Title>메모리스트 with Supabase</Title>
       <section>
         <h1 className="sr-only">메모 리스트 (with Supabase)</h1>
         {loading && <Loading />}
