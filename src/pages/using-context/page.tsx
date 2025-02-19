1fa57feee799e37130dd5ec4c0b6deaaef7587d3
Littlestar0508 <rist9860@gmail.com>
2025-02-19 14:25:36 +0900

    context 학습 환경 구성

---
+++ src/pages/using-context/page.tsx
@@ -0,0 +1,12 @@
+import Title from '@/components/title';
+
+export default function UsingContextPage() {
+  return (
+    <>
+      <Title>컨텍스트를 사용한 상태 공유</Title>
+      <section className="flex flex-col gap-2">
+        <h2 className="text-2xl font-medium">컨텍스트 활용(상태 공유)</h2>
+      </section>
+    </>
+  );
+}
