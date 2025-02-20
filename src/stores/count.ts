4101eb284ea94bc6fc2d01cd4b1ad885e05054a3
Littlestar0508 <rist9860@gmail.com>
2025-02-20 17:41:13 +0900

    zustand 축약 표현법

--- src/stores/count.ts
+++ src/stores/count.ts
@@ -2,38 +2,57 @@ import { create, type StateCreator } from 'zustand';
 
 interface State {
   count: number;
+  step: number;
+  min: number;
+  max: number;
 }
 
 interface Actions {
   increment: () => void;
+  decrement: () => void;
+  update: (value: number) => void;
+  reset: () => void;
+  setStep: (value: number) => void;
+  setMin: (value: number) => void;
+  setMax: (value: number) => void;
 }
 
 type Store = State & Actions;
 
 // 스토어(상태 + 액션 저장소) 생성 함수
-const createStore: StateCreator<Store> = (set, get) => {
+const createStore: StateCreator<Store> = (set) => ({
   // 상태
-  // const count = 0;
+  count: 0,
+  step: 5,
+  min: 0,
+  max: 10,
 
   // 액션
-  const increment = () => {
-    // set() 함수를 사용해 상태 업데이트
-    const state = get();
-    const nextCount = state.count + 1;
-    // 다음 상태 객체 (이전 상태 객체 합성)
-    set({ count: nextCount });
-  };
+  increment: () => set((state) => ({ count: state.count + 1 })),
+  decrement: () => set((state) => ({ count: state.count - 1 })),
+  update: (value) => set({ count: value }),
+  reset: () => set({ count: 0 }),
+  setStep: (value) => set({ step: value }),
+  setMin: (value) => set({ min: value }),
+  setMax: (value) => set({ max: value }),
+});
 
-  // 스토어(store, 저장소)
-  // - 상태(변수)
-  // - 액션(상태 업데이트 함수)
-  return {
-    // 상태
-    count: 0,
-    // 액션
-    increment,
-  };
-};
+export const useCountStore = create<Store>((set) => ({
+  // 상태
+  count: 0,
+  step: 5,
+  min: 0,
+  max: 10,
+
+  // 액션
+  increment: () => set((state) => ({ count: state.count + 1 })),
+  decrement: () => set((state) => ({ count: state.count - 1 })),
+  update: (value) => set({ count: value }),
+  reset: () => set({ count: 0 }),
+  setStep: (value) => set({ step: value }),
+  setMin: (value) => set({ min: value }),
+  setMax: (value) => set({ max: value }),
+}));
 
 // 카운트 상태를 관리하는 스토어를 생성한 후, 외부 컴포넌트에서 호출 가능한 훅 함수를 생성
-export const useCountStore = create(createStore);
+// export const useCountStore = create(createStore);
