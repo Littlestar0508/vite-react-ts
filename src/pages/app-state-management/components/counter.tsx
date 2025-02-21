7c6b1767f895c30b397150c8176f8cfbd2de6f53
Littlestar0508 <rist9860@gmail.com>
2025-02-21 11:04:11 +0900

    구조분해 할당으로 값을 받아오기

--- src/pages/app-state-management/components/counter.tsx
+++ src/pages/app-state-management/components/counter.tsx
@@ -1,15 +1,31 @@
 import { memo } from 'react';
 import CountDisplay from './count-display';
 import CountControllers from './count-controllers';
+import { useCountStore } from '@/stores/count';
 
 function Counter() {
   // 컴포넌트 상태 관리
   // const [count, setCount] = useState(0);
   // const increment = () => setCount((c) => c + 1);
+  const { min, max, count, update } = useCountStore();
+
+  // const update = useCountStore((s) => s.update);
+  // const min = useCountStore((s) => s.min);
+  // const max = useCountStore((s) => s.max);
+  // const count = useCountStore((s) => s.count);
 
   return (
     <div className="flex flex-col gap-2 items-start">
       <CountDisplay />
+      <input
+        type="number"
+        aria-label="카운트 값"
+        className="border bg-white text-black pl-2"
+        value={count}
+        min={min}
+        max={max}
+        onChange={(e) => update(Number(e.currentTarget.value))}
+      />
       <CountControllers />
     </div>
   );
